7f6c7f80fd0d99097faaede73437d6e34f2d4d2d
tien-th <tien.nh205033@sis.hust.edu.vn>
2023-09-21 02:04:38 +0700

    VN_dict

--- configs/textrecog/abinet/abinet-vision_20e_st-an_mj_naver.py
+++ configs/textrecog/abinet/abinet-vision_20e_st-an_mj_naver.py
@@ -37,7 +37,7 @@ test_dataset = dict(
     type='ConcatDataset', datasets=test_list, pipeline=_base_.test_pipeline)
 
 train_dataloader = dict(
-    batch_size=128,
+    batch_size=64,
     num_workers=32,
     persistent_workers=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
